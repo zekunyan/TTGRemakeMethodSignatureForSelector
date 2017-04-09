9260c727a9d8a718174ddd8ff797252b1787ff3c
zekunyan <zekunyan@163.com>
2017-04-09 22:20:15 +0800

    Initial Commit

---
+++ TTGRemakeMethodSignatureForSelector/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TTGRemakeMethodSignatureForSelector
+//
+//  Created by tutuge on 2017/4/9.
+//  Copyright © 2017年 tutuge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
